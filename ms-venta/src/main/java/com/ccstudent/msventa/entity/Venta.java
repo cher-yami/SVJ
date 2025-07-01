4b52993c0d9ac4da0f4ede3b212d49e9a8bed99c
cher-yami <chermanfreefire@gmail.com>
2025-07-01 18:19:00 -0500

    Add files via upload

--- ms-venta/src/main/java/com/ccstudent/msventa/entity/Venta.java
+++ ms-venta/src/main/java/com/ccstudent/msventa/entity/Venta.java
@@ -1,8 +1,5 @@
 package com.ccstudent.msventa.entity;
 
-import com.ccstudent.msventa.dto.ClienteDto;
-import com.ccstudent.msventa.dto.FormaPagoDto;
-import com.ccstudent.msventa.dto.TipoVentaDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,29 +10,25 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="venta")
+@Table(name = "venta")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Venta {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "cliente_id", referencedColumnName = "id",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_CLIENTE_VENTA"))
-    private ClienteDto clienteDto;
+    @Column(name = "cliente_id", nullable = false)
+    private Long clienteId;
 
-    @ManyToOne
-    @JoinColumn(name = "idTipoVenta", referencedColumnName = "id",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_TIPOVENTA_VENTA"))
-    private TipoVentaDto tipoVentaDto;
-    @ManyToOne
-    @JoinColumn(name = "idFormaPago", referencedColumnName = "id",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_FORMAPAGO_VENTA"))
-    private FormaPagoDto formaPagoDto;
+    @Column(name = "idTipoVenta", nullable = false)
+    private Long tipoVentaId;
+
+    @Column(name = "idFormaPago", nullable = false)
+    private Long formaPagoId;
 
     @Column(name = "fechaventa")
     private LocalDateTime fechaventa;
